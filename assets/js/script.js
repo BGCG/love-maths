7d4cf025a6a8d408e7d35a14767d0e1b67cced1e
BGCG <billie.griffith4@gmail.com>
2022-11-13 09:46:10 +0000

    Display addition question to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,35 @@
-function runGame() {
+document.addEventListener("DOMContentLoaded", function () {
+let buttons = document.getElementsByTagName("button");
 
+for(let button of buttons)
+   button.addEventListener("click", function() {
+    if (this.getAttribute("data-type") === "submit") {
+        alert("You clicked Submit!");
+    } else {
+        let gameType = this.getAttribute("data-type");
+       runGame(gameType);
+    }
+    });
+   });
+
+   runGame("addition");
+
+/** 
+ * doc string to describe the function- remember! !
+*/
+function runGame(gameType) {
+
+    //creates two random numbers between 1 and 25 
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+
+    }
 }
 
 function checkAnswer() {
@@ -18,14 +48,17 @@ function incrementWrongAnswer() {
     
 }
 
-function displayAdditionQuestion() {
-    
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('opertor').textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
     
 }
 
-function displayMultipleQuestion() {
+function displayMultiplyQuestion() {
     
 }
